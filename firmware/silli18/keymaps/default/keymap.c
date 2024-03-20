f90b2e85e35090d9b1e51e73f56d63384db93e02
Squalius-cephalus <juho.taskinen@edu.turkuamk.fi>
2024-03-20 19:56:07 +0200

    Firmware added and prebuild Vial firmware too.

---
+++ firmware/silli18/keymaps/default/keymap.c
@@ -0,0 +1,32 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ / │
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ * │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ - │
+     * ├───┼───┼───┼───┤
+     * │ 0 │ . │Ent│ + │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT_ortho_4x5(
+        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
+        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
+        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
+        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS,
+        KC_P0,   KC_PDOT, KC_NO	, KC_NO	
+    )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_A, KC_B), ENCODER_CCW_CW(KC_D, KC_F)  }
+    //                  Encoder 1                                     Encoder 2
+};
+#endif
---
+++ firmware/silli18/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD4, 0x96, 0x0B, 0xC3, 0xDD, 0x29, 0xEE, 0x81}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 1 }
